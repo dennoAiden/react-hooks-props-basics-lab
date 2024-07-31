f9d66d51433c7b6cfaa0cbb334d177620d3b03e8
(dennoAiden) <(kipkiruidennis25@gmail.com)>
2024-07-31 17:18:03 +0300

    app modified

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ function About(props) {
       {props.bio && <p>{props.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
-      <Links github={props.github} linkedin={props.linkedin}/>
+      <Links links={props.links}/>
 
     </div>
   );
--- src/components/Links.js
+++ src/components/Links.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-function Links({github,linkedin}){
-return(
+function Links(props) {
+  return (
     <div id="links">
-        <h3>Links</h3>
-        {github && <a href={github}>{github}</a>}
-        {linkedin && <a href={linkedin}>{linkedin}</a>}        
+      <h3>Links</h3>
+      {props.github && <a href={props.github}>{props.github}</a>}
+      {props.linkedin && <a href={props.linkedin}>{props.linkedin}</a>}
     </div>
-)
+  );
 }
+
 export default Links;
